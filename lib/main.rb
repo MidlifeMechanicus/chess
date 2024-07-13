7f1ede3650fbd7917b5df0ebffe9c941a2d3f9a9
ElectronDragon <electricdragondevelopment@gmail.com>
2024-07-13 10:17:36 +0100

    Create README, Gemfile and initial project notes in main.rb

---
+++ lib/main.rb
@@ -0,0 +1,8 @@
+# We will need a game_board that has pieces as attributes so it can track them.
+# We will need a peiece class, and subclasses.
+# Will have to think about how to save, but that can probably happen at the end.
+# Not TDD, but def use rspec where possible.
+# While it is a terminal game, I still need a board display.
+# Consider chess notation
+# have to select piece at a given node, then check that the move is valid
+# movement rules stored in pieces
